58b60beb5b39d171659c5d4945009da69b45d969
youngjung <crazyyfish@naver.com>
2018-01-03 02:52:42 +0900

    with posecode, Wasserstein, GP

--- DRGAN3D.py
+++ DRGAN3D.py
@@ -40,19 +40,21 @@ class Encoder(nn.Module):
 		utils.initialize_weights(self)
 
 	def forward(self,input):
-		x = self.conv(input)
-		return x
+		fx = self.conv(input)
+		return fx
 
 
 class generator(nn.Module):
 	# Network Architecture is exactly same as in infoGAN (https://arxiv.org/abs/1606.03657)
 	# Architecture : FC1024_BR-FC7x7x128_BR-(64)4dc2s_BR-(1)4dc2s_S
-	def __init__(self, input_dim = 200):
+	def __init__(self, input_dim=200, Npcode=48 ):
+
 		super(generator, self).__init__()
 		self.input_dim = input_dim
+		self.Npcode = Npcode
 
 		self.deconv = nn.Sequential(
-			nn.ConvTranspose3d(200, 512, 4, bias=False),
+			nn.ConvTranspose3d(input_dim+Npcode, 512, 4, bias=False),
 			nn.BatchNorm3d(512),
 			nn.ReLU(),
 			nn.ConvTranspose3d(512, 256, 4, 2, 1, bias=False),
@@ -72,8 +74,10 @@ class generator(nn.Module):
 		)
 		utils.initialize_weights(self)
 
-	def forward(self, x):
-		x = x.view(-1, self.input_dim, 1,1,1 )
+	def forward(self, fx, y_pcode_onehot):
+		fx = fx.view(-1, self.input_dim, 1,1,1 )
+		y_pcode_onehot = y_pcode_onehot.view(-1, self.Npcode, 1,1,1 )
+		x = torch.cat( (fx, y_pcode_onehot), 1 )
 		x = self.deconv(x)
 
 		return x
@@ -81,7 +85,8 @@ class generator(nn.Module):
 class discriminator(nn.Module):
 	# Network Architecture is exactly same as in infoGAN (https://arxiv.org/abs/1606.03657)
 	# Architecture : (64)4c2s-(128)4c2s_BL-FC1024_BL-FC1_S
-	def __init__(self, Nid):
+	def __init__(self, Nid=105, Ncode=48):
+	
 		super(discriminator, self).__init__()
 		self.conv = nn.Sequential(
 			nn.Conv3d(1, 32, 4, 2, 1, bias=False),
@@ -109,16 +114,20 @@ class discriminator(nn.Module):
 		self.convID = nn.Sequential(
 			nn.Conv3d(512, Nid, 4, bias=False),
 		)
+
+		self.convPCode = nn.Sequential(
+			nn.Conv3d(512, Ncode, 4, bias=False),
+		)
 		utils.initialize_weights(self)
 
 	def forward(self, input):
-		fx = self.conv(input)
+		feature = self.conv(input)
 
-		fGAN = self.convGAN( fx ).squeeze(4).squeeze(3).squeeze(2)
-		fid = self.convID( fx )
-		fid = fid.squeeze(4).squeeze(3).squeeze(2)
+		fGAN = self.convGAN( feature ).squeeze(4).squeeze(3).squeeze(2)
+		fid = self.convID( feature ).squeeze(4).squeeze(3).squeeze(2)
+		fcode = self.convPCode( feature ).squeeze(4).squeeze(3).squeeze(2)
 
-		return fGAN, fid
+		return fGAN, fid, fcode
 
 
 class DRGAN3D(object):
@@ -157,6 +166,7 @@ class DRGAN3D(object):
 											shape=(128,128,128)),
 											batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
 			self.Nid = 105
+			self.Npcode = len(self.data_loader.dataset.posecodemap)
 		elif self.dataset == 'IKEA':
 			self.transform = transforms.Compose([transforms.Scale((256, 256)), transforms.ToTensor()])
 			self.data_loader =DataLoader(utils.IKEA(IKEA_data_dir, transform=self.transform), 
@@ -166,13 +176,19 @@ class DRGAN3D(object):
 
 		self.z_dim = 200
 
-		for iB, (sample_x_, _, sample_image_) in enumerate(self.data_loader):
+		for iB, (sample_x_, sample_y_, sample_image_) in enumerate(self.data_loader):
 			self.sample_x_ = sample_x_
 			self.sample_image_ = sample_image_
+			self.sample_y_id_ = sample_y_['id']
+			self.sample_y_pcode_ = sample_y_['pcode']
 			break
 
 		self.sample_x_ = self.sample_x_[:self.test_sample_size,:,:,:,:]
 		self.sample_image_ = self.sample_image_[:self.test_sample_size,:,:,:]
+		self.sample_y_id_ = self.sample_y_id_[:self.test_sample_size]
+		self.sample_y_pcode_ = self.sample_y_pcode_[:self.test_sample_size]
+		self.sample_y_pcode_onehot_ = torch.zeros( self.test_sample_size, self.Npcode )
+		self.sample_y_pcode_onehot_.scatter_(1, self.sample_y_pcode_.view(-1,1), 1)
 #		for iS in range(self.test_sample_size):
 #			fname = os.path.join( self.result_dir, self.dataset, self.model_name, 'sample_%03d.png'%(iS))
 #			imageio.imwrite(fname, self.sample_image_[iS].numpy().transpose(1,2,0))
@@ -183,6 +199,8 @@ class DRGAN3D(object):
 		if self.gpu_mode:
 			self.sample_x_ = Variable( self.sample_x_.cuda(), volatile=True )
 			self.sample_image_ = Variable( self.sample_image_.cuda(), volatile=True )
+			self.sample_y_id_ = Variable( self.sample_y_id_.cuda(), volatile=True )
+			self.sample_y_pcode_onehot_ = Variable( self.sample_y_pcode_onehot_.cuda(), volatile=True )
 			self.sample_z_ = Variable( 
 						torch.normal(torch.zeros(self.test_sample_size, self.z_dim),
 									 torch.ones(self.test_sample_size,self.z_dim)*0.33).cuda(),
@@ -191,10 +209,12 @@ class DRGAN3D(object):
 			self.sample_x_ = Variable( self.sample_x_, volatile=True )
 			self.sample_image_ = Variable( self.sample_image_, volatile=True )
 			self.sample_z_ = Variable(torch.rand((self.batch_size, self.z_dim)), volatile=True)
+			self.sample_y_id_ = Variable( self.sample_y_id_, volatile=True )
+			self.sample_y_pcode_onehot_ = Variable( self.sample_y_pcode_onehot_, volatile=True )
 
 		# networks init
-		self.G = generator(self.z_dim)
-		self.D = discriminator(self.Nid)
+		self.G = generator(self.z_dim, self.Npcode)
+		self.D = discriminator(self.Nid, self.Npcode)
 		self.Enc = Encoder()
 		self.G_optimizer = optim.Adam(self.G.parameters(), lr=args.lrG, betas=(args.beta1, args.beta2))
 		self.D_optimizer = optim.Adam(self.D.parameters(), lr=args.lrD, betas=(args.beta1, args.beta2))
@@ -252,6 +272,14 @@ class DRGAN3D(object):
 					break
 
 				y_id_ = y_['id']
+				y_pcode_ = y_['pcode']
+
+
+				#y_id_onehot_ = torch.zeros( self.batch_size, self.Nid )
+				#y_id_onehot_.scatter_(1, y_id_.view(-1,1), 1)
+				y_pcode_onehot_ = torch.zeros( self.batch_size, self.Npcode )
+				y_pcode_onehot_.scatter_(1, y_pcode_.view(-1,1), 1)
+
 
 				z_ = torch.normal( torch.zeros(self.batch_size, self.z_dim), torch.ones(self.batch_size,self.z_dim) )
 				if self.gpu_mode:
@@ -259,24 +287,31 @@ class DRGAN3D(object):
 					image_ = Variable(image_.cuda())
 					z_ = Variable(z_.cuda())
 					y_id_ = Variable(y_id_.cuda())
+					y_pcode_ = Variable(y_pcode_.cuda())
+					y_pcode_onehot_ = Variable(y_pcode_onehot_.cuda())
 				else:
 					x_ = Variable(x_)
 					image_ = Variable(image_)
 					z_ = Variable(z_)
 					y_id_ = Variable(y_id_)
+					y_pcode_ = Variable(y_pcode_)
+					y_pcode_onehot_ = Variable(y_pcode_onehot_)
 
 				
 				# update D network
 				self.D_optimizer.zero_grad()
 
-				D_real, D_id = self.D(x_)
-				D_loss_real = self.BCE_loss(D_real, self.y_real_)
+				D_real, D_id, D_pcode = self.D(x_)
+				D_loss_real = -torch.mean(D_real)
+#				D_loss_real = self.BCE_loss(D_real, self.y_real_)
 				D_loss_id = self.CE_loss(D_id, y_id_)
+				D_loss_pcode = self.CE_loss(D_pcode, y_pcode_)
 				num_correct_real = torch.sum(D_real>0.5)
 
-				G_ = self.G(z_)
-				D_fake, _ = self.D(G_)
-				D_loss_fake = self.BCE_loss(D_fake, self.y_fake_)
+				G_ = self.G(z_, y_pcode_onehot_)
+				D_fake, _, _ = self.D(G_)
+				D_loss_fake = torch.mean(D_fake)
+#				D_loss_fake = self.BCE_loss(D_fake, self.y_fake_)
 				num_correct_fake = torch.sum(D_fake<0.5)
 
 				""" DRAGAN Loss (Gradient penalty) """
@@ -290,7 +325,7 @@ class DRGAN3D(object):
 						alpha = torch.rand(x_.size())
 						x_hat = Variable(alpha * x_.data + (1 - alpha) * (x_.data + 0.5 * x_.data.std() * torch.rand(x_.size())),
 							requires_grad=True)
-					pred_hat = self.D(x_hat)
+					pred_hat, _, _ = self.D(x_hat)
 					if self.gpu_mode:
 						gradients = grad(outputs=pred_hat, inputs=x_hat, grad_outputs=torch.ones(pred_hat.size()).cuda(),
 									 create_graph=True, retain_graph=True, only_inputs=True)[0]
@@ -300,9 +335,9 @@ class DRGAN3D(object):
 	
 					gradient_penalty = self.lambda_ * ((gradients.view(gradients.size()[0], -1).norm(2, 1) - 1) ** 2).mean()
 	
-					D_loss = D_loss_real + D_loss_id + D_loss_fake + gradient_penalty
+					D_loss = D_loss_real + D_loss_id + D_loss_pcode + D_loss_fake + gradient_penalty
 				else:
-					D_loss = D_loss_real + D_loss_id + D_loss_fake
+					D_loss = D_loss_real + D_loss_id + D_loss_pcode + D_loss_fake
 				D_loss.backward()
 				self.train_hist['D_loss'].append(D_loss.data[0])
 
@@ -324,7 +359,7 @@ class DRGAN3D(object):
 					reparamZ_ = Variable(reparamZ_)
 
 				zey_ = mu + reparamZ_*sigma
-				Gey_ = self.G(zey_)
+				Gey_ = self.G(zey_, y_pcode_onehot_)
 
 				KL_div = 0.5 * torch.sum(mu**2 + sigma**2 - torch.log(1e-8 + sigma**2)-1) / self.batch_size
 				E_loss_MSE = self.MSE_loss( Gey_, x_ )
@@ -345,13 +380,15 @@ class DRGAN3D(object):
 					reparamZ_ = Variable(reparamZ_)
 
 				zey_ = mu + reparamZ_*sigma
-				Gey_ = self.G(zey_)
+				Gey_ = self.G(zey_, y_pcode_onehot_)
 
-				D_fake, D_id = self.D(Gey_)
+				D_fake, D_id, D_pcode = self.D(Gey_)
 
-				G_loss_GAN = self.BCE_loss(D_fake, self.y_real_)
+				G_loss_GAN = -torch.mean(D_fake)
+				#G_loss_GAN = self.BCE_loss(D_fake, self.y_real_)
 				G_loss_MSE = self.MSE_loss(Gey_, x_)
 				G_loss_id = self.CE_loss(D_id, y_id_)
+				G_loss_pcode = self.CE_loss(D_pcode, y_pcode_)
 				G_loss = G_loss_GAN + G_loss_MSE*self.alpha2 + G_loss_id
 				self.train_hist['G_loss'].append(G_loss.data[0])
 
@@ -368,10 +405,10 @@ class DRGAN3D(object):
 
 			self.train_hist['per_epoch_time'].append(time.time() - epoch_start_time)
 			print("dumping x_hat from epoch {}".format(epoch+1))
-			self.dump_x_hat((epoch+1))
-#			self.visualize_results((epoch+1))
 			self.save()
 			utils.loss_plot(self.train_hist, os.path.join(self.save_dir, self.dataset, self.model_name), self.model_name, y_max=10)
+			self.dump_x_hat((epoch+1))
+#			self.visualize_results((epoch+1))
 
 		self.train_hist['total_time'] = time.time() - start_time + self.train_hist['total_time']
 		print("Avg one epoch time: %.2f, total %d epochs time: %.2f" % (np.mean(self.train_hist['per_epoch_time']),
@@ -385,6 +422,9 @@ class DRGAN3D(object):
 	def dump_x_hat(self, epoch, fix=True):
 		self.G.eval()
 
+		result_dir = os.path.join(self.result_dir, self.dataset, self.model_name)
+		if not os.path.exists(result_dir):
+			os.makedirs(result_dir)
 
 		""" fixed image """
 		temp = self.Enc(self.sample_image_)
@@ -395,7 +435,7 @@ class DRGAN3D(object):
 		else:
 			reparamZ_ = Variable(reparamZ_)
 		zey_ = mu + reparamZ_*sigma
-		samples = self.G(zey_)
+		samples = self.G( zey_, self.sample_y_pcode_onehot_ )
 
 		if self.gpu_mode:
 			samples = samples.cpu().data.numpy().squeeze()
