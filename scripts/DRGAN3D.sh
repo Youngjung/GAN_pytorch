982622fa0ac84ea64abda1bcf878d4d766cfdb23
youngjung <crazyyfish@naver.com>
2018-01-18 18:36:03 +0900

    DRGAN3D generates correct shape with wrong color

--- scripts/DRGAN3D.sh
+++ scripts/DRGAN3D.sh
@@ -2,12 +2,16 @@ export CUDA_VISIBLE_DEVICES=0
 python main.py \
  --gan_type DRGAN3D \
  --dataset Bosphorus \
- --comment recon \
+ --comment randomPcode\
  --batch_size 16 \
  --test_sample_size 12 \
  --num_workers 7 \
  --lrD 0.00001 \
+ --lrG 0.0025 \
  --epoch 100
 # --generate True \
 # --use_GP True \
 # --resume True
+
+python utils3D/npy2ply.py --fname results/Bosphorus/DRGAN3D_randomPcode/sampleGT.npy --dir_dest results/Bosphorus/DRGAN3D_randomPcode/
+python utils3D/npy2ply.py --fname results/Bosphorus/DRGAN3D_randomPcode/DRGAN3D_randomPcode_epoch100.npy --dir_dest results/Bosphorus/DRGAN3D_randomPcode/
