d16950654a96d8321660530225e71e2f38c8334e
youngjung <crazyyfish@naver.com>
2018-01-24 22:56:25 +0900

    naive GAN3D_color generates correct shape with blurry color

--- scripts/GAN3D_Bosphorus.sh
+++ scripts/GAN3D_Bosphorus.sh
@@ -1,12 +1,11 @@
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 python main.py \
  --gan_type GAN3D \
  --dataset Bosphorus \
- --comment Bosphorus \
- --batch_size 105 \
- --test_sample_size 4 \
+ --comment color \
+ --batch_size 16 \
+ --test_sample_size 8 \
  --lrG 0.0025 \
  --lrD 0.00001 \
- --num_workers 7 \
+ --num_workers 8 \
  --epoch 100
-# --dataset miniPie
