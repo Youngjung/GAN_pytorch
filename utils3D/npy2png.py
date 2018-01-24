339d9fa78e864e88d96e490ec7e5c6b90eda3bf1
youngjung <crazyyfish@naver.com>
2018-01-24 23:14:08 +0900

    for colored pointcloud npy

--- utils3D/npy2png.py
+++ utils3D/npy2png.py
@@ -1,7 +1,7 @@
 import argparse, os
 import numpy as np
 import visdom
-from visualize import plot_voxel
+from visualize import plot_voxel, plot_colored_voxel
 from multiprocessing import Process
 
 import pdb
@@ -54,12 +54,21 @@ def main():
 
 		g_objects = np.load(fname)
 	
-		for i in range(g_objects.shape[0]):
-			if g_objects[i].max() > 0.5:
-				print( 'plotting epoch {} sample {}...'.format(epoch, i) )
-				plot_voxel(np.squeeze(g_objects[i]>0.5), vis, os.path.join(opts.dir_dest,'_'.join(map(str,[epoch,i]))))
-			else:
-				print( 'max={}'.format(g_objects[i].max()) )
+		if g_objects.ndim == 4:
+			for i in range(g_objects.shape[0]):
+				if g_objects[i].max() > 0.5:
+					print( 'plotting epoch {} sample {}...'.format(epoch, i) )
+					plot_voxel(np.squeeze(g_objects[i]>0.5), vis, os.path.join(opts.dir_dest,'_'.join(map(str,[epoch,i]))))
+				else:
+					print( 'max={}'.format(g_objects[i].max()) )
+		elif g_objects.ndim == 5:
+			for i in range(g_objects.shape[0]):
+				if g_objects[i,0].max() > 0.5:
+					print( 'plotting epoch {} sample {}...'.format(epoch, i) )
+					plot_colored_voxel(np.squeeze(g_objects[i,0]>0.5),g_objects[i,1:4],
+								vis, os.path.join(opts.dir_dest,'_'.join(map(str,[epoch,i]))))
+				else:
+					print( 'max={}'.format(g_objects[i].max()) )
 	
 
 if __name__ == '__main__':
