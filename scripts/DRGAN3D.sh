2f3baaf9ba0786b30b1d3c84785bd2e0bf3119a2
youngjung <crazyyfish@naver.com>
2018-02-04 19:11:42 +0900

    lrG_2e-4

--- scripts/DRGAN3D.sh
+++ scripts/DRGAN3D.sh
@@ -2,16 +2,16 @@ export CUDA_VISIBLE_DEVICES=0
 python main.py \
  --gan_type DRGAN3D \
  --dataset Bosphorus \
- --comment randomPcode\
- --batch_size 16 \
+ --comment lrG_2e-4 \
+ --batch_size 8 \
  --test_sample_size 12 \
- --num_workers 7 \
+ --num_workers 4 \
  --lrD 0.00001 \
- --lrG 0.0025 \
- --epoch 100
+ --lrG 0.0002 \
+ --loss_option dist \
+ --epoch 300 
+# --resume True \
+# --centerBosphorus False \
 # --generate True \
 # --use_GP True \
-# --resume True
 
-python utils3D/npy2ply.py --fname results/Bosphorus/DRGAN3D_randomPcode/sampleGT.npy --dir_dest results/Bosphorus/DRGAN3D_randomPcode/
-python utils3D/npy2ply.py --fname results/Bosphorus/DRGAN3D_randomPcode/DRGAN3D_randomPcode_epoch100.npy --dir_dest results/Bosphorus/DRGAN3D_randomPcode/
