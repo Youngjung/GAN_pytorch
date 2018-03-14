df3ea31716e292ec3b01c7dc11f6b1e7c5cf18cd
youngjung <crazyyfish@naver.com>
2018-03-14 21:37:55 +0900

    scripts misc

--- scripts/DRcycleGAN3D.sh
+++ scripts/DRcycleGAN3D.sh
@@ -2,16 +2,13 @@ export CUDA_VISIBLE_DEVICES=0
 python main.py \
  --gan_type DRcycleGAN3D \
  --dataset Bosphorus \
- --batch_size 6 \
+ --comment lrG_2e-4 \
+ --batch_size 8 \
  --test_sample_size 12 \
- --num_workers 7 \
+ --num_workers 4 \
  --lrD 0.00001 \
- --lrG 0.0025 \
- --epoch 100
+ --lrG 0.0002 \
+ --epoch 300
 # --generate True \
-# --use_GP True \
 # --resume True
 
-python utils3D/npy2ply.py --fname results/Bosphorus/DRcycleGAN3D/sampleGT.npy --dir_dest results/Bosphorus/DRcycleGAN3D/
-python utils3D/npy2ply.py --fname results/Bosphorus/DRcycleGAN3D/DRcycleGAN3D_epoch101_recon.npy --dir_dest results/Bosphorus/DRcycleGAN3D/
-python utils3D/npy2ply.py --fname results/Bosphorus/DRcycleGAN3D/DRcycleGAN3D_epoch101.npy --dir_dest results/Bosphorus/DRcycleGAN3D/
--- scripts/GAN3D_Bosphorus.sh
+++ scripts/GAN3D_Bosphorus.sh
@@ -2,8 +2,8 @@ export CUDA_VISIBLE_DEVICES=0
 python main.py \
  --gan_type GAN3D \
  --dataset Bosphorus \
- --comment color \
- --batch_size 16 \
+ --comment projected \
+ --batch_size 24 \
  --test_sample_size 8 \
  --lrG 0.0025 \
  --lrD 0.00001 \
