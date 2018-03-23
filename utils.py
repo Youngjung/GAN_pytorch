761edf4c1a0cda64ec814c7bd67aa96497b9865d
youngjung <crazyyfish@naver.com>
2018-03-23 18:25:43 +0900

    initialize_weights()

--- utils.py
+++ utils.py
@@ -497,10 +497,12 @@ def initialize_weights(net):
 	for m in net.modules():
 		if isinstance(m, nn.Conv2d):
 			m.weight.data.normal_(0, 0.02)
-			m.bias.data.zero_()
+			if m.bias is not None:
+				m.bias.data.zero_()
 		elif isinstance(m, nn.ConvTranspose2d):
 			m.weight.data.normal_(0, 0.02)
-#			m.bias.data.zero_()
+			if m.bias is not None:
+				m.bias.data.zero_()
 		elif isinstance(m, nn.Conv3d):
 			nn.init.xavier_uniform(m.weight)
 		elif isinstance(m, nn.ConvTranspose3d):
